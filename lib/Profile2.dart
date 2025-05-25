7d9b1cb15f04fdf816d80c23dc0b469a38876dfd
FarhanSFix <farhansfixe@gmail.com>
2025-05-25 21:59:40 +0700

    Membuat Profile Fixes #1

---
+++ lib/Profile2.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Profile2 extends StatefulWidget {
+  const Profile2({super.key});
+
+  @override
+  State<Profile2> createState() => _Profile2State();
+}
+
+class _Profile2State extends State<Profile2> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Halaman Profile2")),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text("Ini adalah Halaman Profile 2"),
+          CircleAvatar(backgroundColor: Colors.blue),
+        ],
+      ),
+    );
+  }
+}
